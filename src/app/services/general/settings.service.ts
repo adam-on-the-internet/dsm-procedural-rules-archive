fb91baf02e08d2b91df7aec0275efe46dff197fd
Adam Callanan <a.callanan@sbcglobal.net>
2023-02-06 12:58:17 -0600

    update

--- src/app/services/general/settings.service.ts
+++ src/app/services/general/settings.service.ts
@@ -5,5 +5,5 @@ import {Injectable} from "@angular/core";
 })
 export class SettingsService {
   public showNav = true;
-  public asOfDate = "January 26, 2022";
+  public asOfDate = "January 23, 2023";
 }
